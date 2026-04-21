6a2af7a36bde43cb9f200310fbe897777d81801d
Vivian Beck <averse.abfun@gmail.com>
2026-04-21 13:31:25 -0500

    update a bunch of misc stuff including some very old stuff that i updated a while ago and didn't commit

--- TeamCode/team_code_rust/src/lib.rs
+++ TeamCode/team_code_rust/src/lib.rs
@@ -2,38 +2,22 @@
 
 use ftc::{ftc, hardware::DcMotor};
 
-// /// Example linear op mode.
-// #[ftc(
-//     name = "Example: My Linear Op Mode",
-//     linear,
-//     teleop,
-//     group = "Example",
-// )]
-// fn my_linear_op_mode(ctx: &ftc::FtcContext) {
-//     // equivalent to hardwareMap.get(DcMotor.class, "motor") in Java:
-//     // let motor = ctx.hardware().get::<DcMotor>("motor");
-//     // motor.set_direction(ftc::hardware::Direction::Forward);
+/// Example linear op mode.
+#[ftc(name = "Example: My Linear Op Mode", linear, teleop, group = "Example")]
+fn my_linear_op_mode(ctx: &ftc::FtcContext) {
+    // equivalent to hardwareMap.get(DcMotor.class, "motor") in Java
+    // also fun fact: the syntax `::<T>` is affectionately called the turbofish!
+    let motor = ctx.hardware().get::<DcMotor>("motor");
+    motor.set_direction(ftc::hardware::Direction::Forward);
 
-//     // ctx.telemetry().add_data("Status", "Initalized");
-//     // ctx.telemetry().update();
+    ctx.telemetry().add_data("Status", "Initalized");
+    ctx.telemetry().update();
 
-//     // ctx.wait_for_start();
+    ctx.wait_for_start();
 
-//     // // ctx.running() instead of opModeIsActive()
+    // ctx.running() instead of opModeIsActive()
 
-//     // motor.set_power(0.5);
-//     // ctx.sleep_s(2.0);
-//     // motor.set_power(0.0);
-// }
-
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_org_firstinspires_ftc_teamcode_MyLinearOpMode_runOpMode<
-    'local,
->(
-    mut unowned_env: ::ftc::jni::EnvUnowned<'local>,
-    this: ::ftc::jni::objects::JObject<'local>,
-) {
-    let outcome = unowned_env.with_env_no_catch(|env| -> ::ftc::jni::errors::Result<_> {
-        Ok(())
-    });
+    motor.set_power(0.5);
+    ctx.sleep_s(2.0);
+    motor.set_power(0.0);
 }
